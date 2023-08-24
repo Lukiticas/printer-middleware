9023b73215de1eadc3a4d91eff4b8e7dfff902da
Lucas Matheus <Lucasmathues416@outlook.com>
2023-08-24 14:01:45 -0300

    ssaca

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const http = require('http');
+
+const get = (url) => {
+    return new Promise((resolve, reject) => {
+        http.get(url, (res) => {
+        let body = '';
+        res.on('data', (chunk) => { body += chunk; });
+        res.on('end', () => { resolve(JSON.parse(body)); });
+        }).on('error', (err) => { reject(err); });
+    });
+}
 
 router.get('/', function(req, res, next) {
-  res.json({ message: 'Hello World!' })
+  return get('http://localhost:9100').then((response) => {
+    return  res.json({ response })
+  })
 });
 
 module.exports = router;
